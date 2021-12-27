7a7b1ce2299d015b775db16cc03ce20872025ffc
Mussejusse2 <mussejusse@gmail.com>
2021-12-27 16:21:17 +0000

    Changed colours to only produce pastel colour

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var randomColour = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+//var randomColour = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+
+var randomColour = "hsla(" + ~~(360 * Math.random()) + "," + "70%," + "80%,1)"
 document.documentElement.style.setProperty('--bgcolour', randomColour);
