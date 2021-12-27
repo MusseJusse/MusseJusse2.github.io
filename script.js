8da46bb3bdbd46a262002e1a07b992494b2651f5
Mussejusse2 <mussejusse@gmail.com>
2021-12-27 16:24:34 +0000

    Removed comment

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-//var randomColour = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-
 var randomColour = "hsla(" + ~~(360 * Math.random()) + "," + "70%," + "80%,1)"
 document.documentElement.style.setProperty('--bgcolour', randomColour);
