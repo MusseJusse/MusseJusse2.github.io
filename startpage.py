16ae82dc50989699f6dbec0cd28b8dcbc236a571
mussejusse2 <mussejusse@gmail.com>
2022-08-26 15:05:04 +0100

    Added uni stuff

--- startpage.py
+++ startpage.py
@@ -1,7 +1,7 @@
 import re
 
-importPath = r"C:\\Users\\Musse\\Documents\\Github\\MusseJusse2.github.io\\content.txt"
-outputPath = r"C:\\Users\\Musse\\Documents\\Github\\MusseJusse2.github.io\\index.html"
+importPath = r"C:\\Users\\Musse\\Documents\\Projs\\MusseJusse2.github.io\\content.txt"
+outputPath = r"C:\\Users\\Musse\\Documents\\Projs\\MusseJusse2.github.io\\index.html"
 user = "musse"
 main = user + "@home &gt; "
 title = main + "startpage"
